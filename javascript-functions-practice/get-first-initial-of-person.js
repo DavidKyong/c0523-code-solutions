7c4eb8db29562490cd03ed90512e48b17620001f
David Kyong <dvdkyong@gmail.com>
2023-05-11 20:32:36 +0000

    Completed javascript-functions-practice exercise

--- javascript-functions-practice/get-first-initial-of-person.js
+++ javascript-functions-practice/get-first-initial-of-person.js
@@ -7,5 +7,5 @@ const lea2 = {
   firstName: 'Lea',
   lastName: 'Verou',
 };
-const getFirstInitialOfPersonResults = getFirstInitialOfPerson(lea);
+const getFirstInitialOfPersonResults = getFirstInitialOfPerson(lea2);
 console.log(getFirstInitialOfPersonResults);
--- javascript-functions-practice/get-initials-of-person.js
+++ javascript-functions-practice/get-initials-of-person.js
@@ -1,11 +1,11 @@
 /* exported getInitialsOfPerson */
 function getInitialsOfPerson(person) {
-  const lea = {
-    firstName: 'Lea',
-    lastName: 'Verou',
-  };
   return person.firstName.charAt(0) + person.lastName.charAt(0);
 }
+const lea = {
+  firstName: 'Lea',
+  lastName: 'Verou',
+};
 
 const getInitialsOfPersonResults = getInitialsOfPerson(lea);
 console.log(getInitialsOfPersonResults);
