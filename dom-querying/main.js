0ab2ac487c98109ce4e672032ecf0163d7d765e3
David Kyong <dvdkyong@gmail.com>
2023-05-22 21:13:44 +0000

    Completed dom-querying exercise

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -1,14 +1,14 @@
 console.log('hello, world');
-let heading = document.querySelector('h1');
+const heading = document.querySelector('h1');
 console.log(heading);
 console.dir(heading);
-let explanation = document.querySelector('#explanation');
+const explanation = document.querySelector('#explanation');
 console.log(explanation);
 console.dir(explanation);
-let hint = document.querySelector('.hint');
+const hint = document.querySelector('.hint');
 console.log(hint);
 console.dir(hint);
-let paragraphs = document.querySelectorAll('p');
+const paragraphs = document.querySelectorAll('p');
 console.log(paragraphs);
-let links = document.querySelectorAll('.example-link')
+const links = document.querySelectorAll('.example-link')
 console.log(links);
