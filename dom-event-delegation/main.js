b3546b384f93ab94a94b19846440470bf9865eb4
David Kyong <dvdkyong@gmail.com>
2023-05-25 20:40:16 +0000

    Completed dom-event-delegation exercise

---
+++ dom-event-delegation/main.js
@@ -0,0 +1,18 @@
+const $taskList = document.querySelector('.task-list');
+
+
+
+
+function singleClick(event) {
+  console.log('event.target:', event.target);
+  console.log('event.target.tagName:', event.target.tagName);
+  if (event.target && event.target.nodeName === "BUTTON") {
+   console.log('closest .task-list-item', event.target.closest('.task-list-item'));
+  }
+  const removing = event.target.closest('.task-list-item');
+  removing.remove();
+}
+
+
+
+$taskList.addEventListener('click', singleClick);
