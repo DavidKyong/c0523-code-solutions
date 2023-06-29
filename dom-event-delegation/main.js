851c383d2c113d54c22db5718b7d9f038b2c11c8
David Kyong <dvdkyong@gmail.com>
2023-06-29 04:01:41 +0000

    Fixed the error

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -1,18 +1,16 @@
 const $taskList = document.querySelector('.task-list');
 
-
-
-
 function singleClick(event) {
   console.log('event.target:', event.target);
   console.log('event.target.tagName:', event.target.tagName);
-  if (event.target && event.target.nodeName === "BUTTON") {
-   console.log('closest .task-list-item', event.target.closest('.task-list-item'));
+  if (event.target && event.target.nodeName === 'BUTTON') {
+    console.log(
+      'closest .task-list-item',
+      event.target.closest('.task-list-item')
+    );
+    const $removing = event.target.closest('.task-list-item');
+    $removing.remove();
   }
-  const $removing = event.target.closest('.task-list-item');
-  removing.remove();
 }
-
-
 
 $taskList.addEventListener('click', singleClick);
